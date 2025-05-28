3ba6de5b9deffa9dacb84167714b39f562270490
Sych-1337 <53253377+Sych-1337@users.noreply.github.com>
2025-05-28 10:35:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 APP_CONFIG = {
     "app1": {
-        "keitaro_url": "https://firtsoneballs.xyz/stream_app1"
+        "keitaro_url": "https://firtsoneballs.xyz/XD2gBKJv"
     },
     "app2": {
-        "keitaro_url": "https://firtsoneballs.xyz/stream_app2"
+        "keitaro_url": "https://fortunetaskersweetfate.xyz/Y46ndq"
     },
     "app3": {
         "keitaro_url": "https://firtsoneballs.xyz/stream_app3"
