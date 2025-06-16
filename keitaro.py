f8151894b574b64a21bd8f9c86d0472c16d53cbf
Sych-1337 <53253377+Sych-1337@users.noreply.github.com>
2025-06-16 10:39:09 +0300

    Update keitaro.py

--- keitaro.py
+++ keitaro.py
@@ -1,7 +1,7 @@
 import httpx
 from config import APP_CONFIG
 
-async def get_offer_url(user_id: str, campaign: str, app_name: str) -> str | None:
+async def get_offer_url(user_id: str, campaign: str, app_name: str, tracking_id: str = "", sub_ids: dict = {}) -> str | None:
     app = APP_CONFIG.get(app_name)
     if not app:
         return None
@@ -10,11 +10,19 @@ async def get_offer_url(user_id: str, campaign: str, app_name: str) -> str | Non
     if not stream_url:
         return None
 
+    params = {
+        "sub_id": user_id,
+        "campaign": campaign,
+        "tracking_id": tracking_id
+    }
+
+    # Добавим sub_id_2 ... sub_id_8
+    for key, value in sub_ids.items():
+        if value:
+            params[key] = value
+
     async with httpx.AsyncClient(follow_redirects=False) as client:
-        response = await client.get(
-            stream_url,
-            params={"sub_id": user_id, "campaign": campaign}
-        )
+        response = await client.get(stream_url, params=params)
         if "location" in response.headers:
             return response.headers["location"]
 
